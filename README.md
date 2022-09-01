0b55a8534d59bb3d445dd2c3a65c7369e68a2bbe
Mikail Thoriq <mikail.thoriq@gmail.com>
2022-09-01 22:33:58 +0700

    first push

---
+++ README.md
@@ -0,0 +1,15 @@
+# ngirit-cuy
+
+It downloads original image and transforms it with [Sharp](https://github.com/lovell/sharp) on the
+fly without saving images on disk.
+
+
+## if you want to use authentication 
+
+1. make `.env` 
+
+2. with content :
+```
+USER=anything
+PASS=securepassword
+```
---
+++ src/authenticate.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const auth = require('basic-auth')
+const LOGIN = process.env.USER
+const PASSWORD = process.env.PASS
+
+function authenticate(req, res, next) {
+  if (LOGIN && PASSWORD) {
+    const credentials = auth(req)
+    if (!credentials || credentials.name !== LOGIN || credentials.pass !== PASSWORD) {
+      res.setHeader('WWW-Authenticate', `Basic realm="ngirit-cuy Compression Service"`)
+
+      return res.status(401).end('Access denied')
+    }
+  }
+
+  next()
+}
+
+module.exports = authenticate
